6ac15b1aeabc6ae92fcb98c7e7acf5da67d07b96
Andreas Schiffler <schiffler@roschi.eu>
2023-01-25 22:09:55 +0000

    first working

--- main.cpp
+++ main.cpp
@@ -1,64 +1,121 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <string>
-#include <thread>	// For sleep
-#include <atomic>
 #include <chrono>
 #include <cstring>
-#include "paho.mqtt.cpp/src/mqtt/async_client.h"
+#include "mqtt/async_client.h"
 
-using namespace std;
+const std::string DFLT_SERVER_ADDRESS	{ "wss://localhost:8080" };
+const std::string DFLT_CLIENT_ID		{ "ssl_publish_cpp" };
 
-const string DFLT_SERVER_ADDRESS { "tcp://localhost:1883" };
+const std::string KEY_STORE				{ "tls.key" };
+const std::string TRUST_STORE			{ "tls.crt" };
 
-const string TOPIC { "test" };
-const int QOS = 1;
+const std::string LWT_TOPIC				{ "events/disconnect" };
+const std::string LWT_PAYLOAD			{ "Last will and testament." };
 
-const char* PAYLOADS[] = {
-	"Hello World!",
-	"Hi there!",
-	"Is anyone listening?",
-	"Someone is always listening.",
-	nullptr
-};
-
-const auto TIMEOUT = std::chrono::seconds(60);
+const int  QOS = 1;
+const auto TIMEOUT = std::chrono::seconds(10);
 
 /////////////////////////////////////////////////////////////////////////////
 
+/**
+ * A callback class for use with the main MQTT client.
+ */
+class callback : public virtual mqtt::callback
+{
+public:
+	void connection_lost(const std::string& cause) override {
+		std::cout << "\nConnection lost" << std::endl;
+		if (!cause.empty())
+			std::cout << "\tcause: " << cause << std::endl;
+	}
+
+	void delivery_complete(mqtt::delivery_token_ptr tok) override {
+		std::cout << "\tDelivery complete for token: "
+			<< (tok ? tok->get_message_id() : -1) << std::endl;
+	}
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
 int main(int argc, char* argv[])
 {
-	string address = (argc > 1) ? string(argv[1]) : DFLT_SERVER_ADDRESS;
+	string	address  = (argc > 1) ? string(argv[1]) : DFLT_SERVER_ADDRESS,
+			clientID = (argc > 2) ? string(argv[2]) : DFLT_CLIENT_ID;
+
+	// Note that we don't actually need to open the trust or key stores.
+	// We just need a quick, portable way to check that they exist.
+	{
+		ifstream tstore(TRUST_STORE);
+		if (!tstore) {
+			cerr << "The trust store file does not exist: " << TRUST_STORE << endl;
+			cerr << "  Get a copy from \"paho.mqtt.c/test/ssl/test-root-ca.crt\"" << endl;;
+			return 1;
+		}
+
+		ifstream kstore(KEY_STORE);
+		if (!kstore) {
+			cerr << "The key store file does not exist: " << KEY_STORE << endl;
+			cerr << "  Get a copy from \"paho.mqtt.c/test/ssl/client.pem\"" << endl;
+			return 1;
+		}
+    }
 
 	cout << "Initializing for server '" << address << "'..." << endl;
-	mqtt::async_client cli(address, "");
+	mqtt::async_client client(address, clientID);
+
+	callback cb;
+	client.set_callback(cb);
+
+	// Build the connect options, including SSL and a LWT message.
+
+	auto sslopts = mqtt::ssl_options_builder()
+					   .trust_store(TRUST_STORE)
+					   .enable_server_cert_auth(false) // will fail for self signed
+					   .error_handler([](const std::string& msg) {
+						   std::cerr << "SSL Error: " << msg << std::endl;
+					   })
+					   .finalize();
+
+	auto willmsg = mqtt::message(LWT_TOPIC, LWT_PAYLOAD, QOS, true);
+
+	auto connopts = mqtt::connect_options_builder()
+					    .user_name("testuser")
+					    .password("testpassword")
+					    .will(std::move(willmsg))
+						.ssl(std::move(sslopts))
+						.finalize();
 
 	cout << "  ...OK" << endl;
 
 	try {
+		// Connect using SSL/TLS
+
 		cout << "\nConnecting..." << endl;
-		cli.connect()->wait();
+		mqtt::token_ptr conntok = client.connect(connopts);
+		cout << "Waiting for the connection..." << endl;
+		conntok->wait();
 		cout << "  ...OK" << endl;
 
-		cout << "\nPublishing messages..." << endl;
+		// Send a message
 
-		mqtt::topic top(cli, "test", QOS);
-		mqtt::token_ptr tok;
-
-		size_t i = 0;
-		while (PAYLOADS[i]) {
-			tok = top.publish(PAYLOADS[i++]);
-		}
-		tok->wait();	// Just wait for the last one to complete.
-		cout << "OK" << endl;
+		cout << "\nSending message..." << endl;
+		auto msg = mqtt::make_message("hello", "Hello secure C++ world!", QOS, false);
+		client.publish(msg)->wait_for(TIMEOUT);
+		cout << "  ...OK" << endl;
 
 		// Disconnect
+
 		cout << "\nDisconnecting..." << endl;
-		cli.disconnect()->wait();
+		client.disconnect()->wait();
 		cout << "  ...OK" << endl;
 	}
 	catch (const mqtt::exception& exc) {
-		cerr << exc << endl;
+		cerr << exc.what() << endl;
 		return 1;
 	}
 
