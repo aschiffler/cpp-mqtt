04cb3530f31df060e277aadbc4660267be2f1186
Andreas Schiffler <schiffler@roschi.eu>
2023-01-25 21:27:58 +0000

    work setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <thread>	// For sleep
+#include <atomic>
+#include <chrono>
+#include <cstring>
+#include "paho.mqtt.cpp/src/mqtt/async_client.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+const string DFLT_SERVER_ADDRESS { "tcp://localhost:1883" };
+
+const string TOPIC { "test" };
+const int QOS = 1;
+
+const char* PAYLOADS[] = {
+	"Hello World!",
+	"Hi there!",
+	"Is anyone listening?",
+	"Someone is always listening.",
+	nullptr
+};
+
+const auto TIMEOUT = std::chrono::seconds(60);
+
+/////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[])
+{
+	string address = (argc > 1) ? string(argv[1]) : DFLT_SERVER_ADDRESS;
+
+	cout << "Initializing for server '" << address << "'..." << endl;
+	mqtt::async_client cli(address, "");
+
+	cout << "  ...OK" << endl;
+
+	try {
+		cout << "\nConnecting..." << endl;
+		cli.connect()->wait();
+		cout << "  ...OK" << endl;
+
+		cout << "\nPublishing messages..." << endl;
+
+		mqtt::topic top(cli, "test", QOS);
+		mqtt::token_ptr tok;
+
+		size_t i = 0;
+		while (PAYLOADS[i]) {
+			tok = top.publish(PAYLOADS[i++]);
+		}
+		tok->wait();	// Just wait for the last one to complete.
+		cout << "OK" << endl;
+
+		// Disconnect
+		cout << "\nDisconnecting..." << endl;
+		cli.disconnect()->wait();
+		cout << "  ...OK" << endl;
+	}
+	catch (const mqtt::exception& exc) {
+		cerr << exc << endl;
+		return 1;
+	}
+
+ 	return 0;
 }
